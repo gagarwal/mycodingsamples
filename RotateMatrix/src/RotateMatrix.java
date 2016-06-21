2555a85f664c48c0832a6739427e39ca9f780be4
bealearnerforever <BinduKallamadi@Bindus-MacBook-Pro.local>
2016-06-21 02:06:43 -0500

    Added rotateMatrix

--- RotateMatrix/src/RotateMatrix.java
+++ RotateMatrix/src/RotateMatrix.java
@@ -2,38 +2,58 @@ import helper.AssortedMethods;
 
 public class RotateMatrix {
 
-	public static void rotate(int[][] matrix, int n) {
-		for (int layer = 0; layer < n / 2; ++layer) {
-			int first = layer;
-			int last = n - 1 - layer;
-			for (int i = first; i < last; ++i) {
-				int offset = i - first;
-				int top = matrix[first][i]; // save top
-
-				// left -> top
-				matrix[first][i] = matrix[last - offset][first];
-
-				// bottom -> left
-				matrix[last - offset][first] = matrix[last][last - offset];
-
-				// right -> bottom
-				matrix[last][last - offset] = matrix[i][last];
-
-				// top -> right
-				matrix[i][last] = top; // right <- saved top
-				
-				System.out.println();
-				AssortedMethods.printMatrix(matrix);
-			}
-		}
-	}
+//	public static void rotate(int[][] matrix, int n) {
+//		for (int layer = 0; layer < n / 2; ++layer) {
+//			int first = layer;
+//			int last = n - 1 - layer;
+//			for (int i = first; i < last; ++i) {
+//				int offset = i - first;
+//				int top = matrix[first][i]; // save top
+//
+//				// left -> top
+//				matrix[first][i] = matrix[last - offset][first];
+//
+//				// bottom -> left
+//				matrix[last - offset][first] = matrix[last][last - offset];
+//
+//				// right -> bottom
+//				matrix[last][last - offset] = matrix[i][last];
+//
+//				// top -> right
+//				matrix[i][last] = top; // right <- saved top
+//				
+//				System.out.println();
+//				AssortedMethods.printMatrix(matrix);
+//			}
+//		}
+//	}
 
 	public static void main(String[] args) {
-		int[][] matrix = AssortedMethods.randomMatrix(4, 4, 0, 16);
-		AssortedMethods.printMatrix(matrix);
-		rotate(matrix, 4);
+		int[][] matrix = AssortedMethods.randomMatrix(3, 3, 0, 9);
+		//AssortedMethods.printMatrix(matrix);
+		rotate(matrix);
 		System.out.println();
-		AssortedMethods.printMatrix(matrix);
+		//AssortedMethods.printMatrix(matrix);
+	
 	}
+	public static void rotate(int[][] matrix)
+    {
+		AssortedMethods.printMatrix(matrix);
+		System.out.println();
+	    int n = matrix.length;
+	    for (int i = 0; i < Math.ceil(((double) n) / 2.0); i++)
+	    {
+		    for (int j = 0; j <  n / 2; j++)
+		    {
+			    int temp = matrix[i][j];
+			    matrix[i][j] = matrix[n-1-j][i];
+			    matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+			    matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+			    matrix[j][n-1-i] = temp;
+		    }
+		    
+	   }
+	     AssortedMethods.printMatrix(matrix);
+    }
 
 }
