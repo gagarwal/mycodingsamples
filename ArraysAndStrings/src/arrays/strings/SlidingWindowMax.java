312208ad075cae5f53d8076cf19d03c0e23c7561
bealearnerforever <BinduKallamadi@Bindus-MacBook-Pro.local>
2016-07-13 01:04:04 -0500

    Typo in sliding max window

--- ArraysAndStrings/src/arrays/strings/SlidingWindowMax.java
+++ ArraysAndStrings/src/arrays/strings/SlidingWindowMax.java
@@ -5,25 +5,26 @@ import java.util.Deque;
 
 public class SlidingWindowMax 
 {
-    public static int[] maxSlidingWindow(int[] input, int k) 
+	public static int[] maxSlidingWindow(int[] input, int k) 
     {
-        if (input == null || k <= 0) return new int[0];
+		if (input == null || k <= 0) return new int[0];
 		
 		int[] output = new int[input.length-k+1];
-		
 		int opIndex = 0;
-		Deque<Integer> queue = new ArrayDeque<>(); // store index
-
+		
+		// store index
+		Deque<Integer> queue = new ArrayDeque<>();
 		for (int i = 0; i < input.length; i++) 
 		{
 			// remove numbers out of range k
-			if (!queue.isEmpty() &&  i - queue.peek()  > k+1 ) 
+			if (!queue.isEmpty() && i - queue.peek() > k-1)
 				queue.poll();
 
 			// remove smaller numbers in k range as they are useless
-			while (!queue.isEmpty() && input[queue.peekLast()] < input[i])
+			while (!queue.isEmpty() && input[queue.peekLast()] < input[i]) {
 				queue.pollLast();
-
+			}
+			// q contains index... r contains content
 			queue.offer(i);
 			if (i >= k - 1) 
 				output[opIndex++] = input[queue.peek()];
@@ -33,7 +34,8 @@ public class SlidingWindowMax
     
     public static void main(String args[])
     {
-    	int[] input = {2,3,4,1, -100, 8, 100};
+    	int[] input = {9, 11};
+    			
     	int[] output = SlidingWindowMax.maxSlidingWindow(input, 3);
     	
     	for(int i: output)
