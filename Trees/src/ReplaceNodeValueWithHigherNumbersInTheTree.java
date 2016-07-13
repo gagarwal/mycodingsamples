868893294ff2bb3100753a8752a9877afa5013a8
bealearnerforever <BinduKallamadi@Bindus-MacBook-Pro.local>
2016-07-13 02:52:26 -0500

    Replace node with sum of all it's nodes with high values

---
+++ Trees/src/ReplaceNodeValueWithHigherNumbersInTheTree.java
@@ -0,0 +1,29 @@
+import helper.TreeNode;
+
+public class ReplaceNodeValueWithHigherNumbersInTheTree
+{
+	public static int sumAllNodes(TreeNode node)
+	{
+		if(node == null) return 0;
+		return node.data + sumAllNodes(node.left) + sumAllNodes(node.right);
+	}
+	
+	public static void replaceNodeValueWithHigherNumbersInTheTree(TreeNode node, int total, TreeNode root)
+	{
+		if(node == null) return;
+		
+		int leftSum = sumAllNodes(node.left);
+		int currentData = node.data;
+		node.data = total - leftSum - currentData;
+		
+		replaceNodeValueWithHigherNumbersInTheTree(node.right, total - leftSum - currentData, root);
+		replaceNodeValueWithHigherNumbersInTheTree(node.left, total , root);
+	}
+	
+	public static void main(String[] args) {
+		int[] array = { 0, 1, 3, 4, 7, 8 ,10 };
+		TreeNode root = TreeNode.createMinimalBST(array);
+		int sumTotal = sumAllNodes(root);
+		replaceNodeValueWithHigherNumbersInTheTree(root, sumTotal, root);
+	}
+}
