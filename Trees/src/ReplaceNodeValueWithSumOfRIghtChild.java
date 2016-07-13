a60f245af04736f31d309a7cceb1320d887c1882
bealearnerforever <BinduKallamadi@Bindus-MacBook-Pro.local>
2016-07-13 02:52:05 -0500

    Replace node with sum of all it's right children values

---
+++ Trees/src/ReplaceNodeValueWithSumOfRIghtChild.java
@@ -0,0 +1,39 @@
+import helper.BTreePrinter;
+import helper.TreeNode;
+
+public class ReplaceNodeValueWithSumOfRIghtChild 
+{
+
+	public static void nodeGetSumGreater(TreeNode node)
+	{
+		if(node == null) return ;
+		
+		node.data = getSum(node.right);
+		
+		nodeGetSumGreater(node.left);
+		nodeGetSumGreater(node.right);
+		
+		return;
+	}
+	
+	private static int getSum(TreeNode node)
+	{
+		int sum = 0;
+		while(node!=null)
+		{
+			sum = sum + node.data;
+			node = node.right;
+		}
+			
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		// Create balanced tree
+		int[] array = { 0, 1, 3, 4, 7, 8 ,10 };
+		TreeNode root = TreeNode.createMinimalBST(array);
+		BTreePrinter.printNode(root);
+		nodeGetSumGreater(root);
+		BTreePrinter.printNode(root);
+	}
+}
