17499ba6a9b84a44edee20f51a430c628152112a
bealearnerforever <BinduKallamadi@Bindus-MacBook-Pro.local>
2016-07-15 21:57:48 -0500

    Add two numbers without arithmetic operators

---
+++ SumNumbers/src/SumNumbers.java
@@ -0,0 +1,25 @@
+
+public class SumNumbers
+{
+	 public static int getSum(int x, int y) 
+	 {
+	        // Iterate till there is no carry  
+	        while (y != 0)
+	        {
+	            // carry now contains common set bits of x and y
+	             int carry = x & y;  
+	 
+	            // Sum of bits of x and y where at least one of the bits is not set
+	            x = x ^ y; 
+	 
+	            // Carry is shifted by one so that adding it to x gives the required sum
+	            y = carry << 1;
+	         }
+	         return x;
+     }
+	 
+	 public static void main(String args[])
+	 {
+		 System.out.println(getSum(13,4));
+	 }
+}
