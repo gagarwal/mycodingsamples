81f9ea73ddafe059914d104fc155a43f7f6de891
bealearnerforever <BinduKallamadi@Bindus-MacBook-Pro.local>
2016-07-20 01:54:47 -0500

    formatting

--- LinkedList/src/LinkedListReversalIterative.java
+++ LinkedList/src/LinkedListReversalIterative.java
@@ -1,6 +1,9 @@
 
-public class LinkedListReversalIterative {
-	public static void main(String[] args) {
+public class LinkedListReversalIterative 
+{
+	@SuppressWarnings("unused")
+	public static void main(String[] args)
+	{
 		LinkedListNode lA1 = new LinkedListNode(2, null, null);
 		LinkedListNode lA2 = new LinkedListNode(3, null, lA1);
 		LinkedListNode lA3 = new LinkedListNode(9, null, lA2);
