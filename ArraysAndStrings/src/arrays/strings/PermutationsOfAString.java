a0d92e348136ea271242d223239f07ef83ae8397
bealearnerforever <BinduKallamadi@Bindus-MacBook-Pro.local>
2016-07-21 02:48:33 -0500

    print Permutations

---
+++ ArraysAndStrings/src/arrays/strings/PermutationsOfAString.java
@@ -0,0 +1,19 @@
+package arrays.strings;
+
+public class PermutationsOfAString 
+{
+	private static void printPermutations(String prefix, String rest)
+	{
+		int restLength = rest.length();
+		if(restLength == 0)
+			System.out.println(prefix);
+
+		for(int i = 0; i<restLength; i++)
+			printPermutations(prefix+rest.charAt(i),  rest.substring(0,i)+rest.substring(i+1, restLength));
+	}
+	
+	public static void main(String[] args) 
+	{
+		printPermutations("","ab");
+	}
+}
